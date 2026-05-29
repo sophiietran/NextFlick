070442415797d8f4092b2d17465f19516800fe0e
Sophie <sophiie.tran@gmail.com>
2026-05-29 11:34:02 -0500

    project structure cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,19 @@
 import "./App.css";
 import React from "react";
-import MovieSearch from "./MovieSearch"
-import Recommendations from "./Recommendations";
-
-type MovieSuggestion = {
-  id: number;
-  title: string;
-};
+import MovieSearch from "./components/MovieSearch"
+import Recommendations from "./components/Recommendations";
+import SelectedMovieInfo from "./components/SelectedMovieInfo";
+import type { MovieDetails } from "./types/movieTypes";
 
 export default function App() {
 
-  const [selectedMovie, setSelectedMovie] = React.useState<MovieSuggestion | null>(null);
+  const [selectedMovie, setSelectedMovie] = React.useState<MovieDetails | null>(null);
 
   return (
     <main className="app">
       <div className="app-content">
         <MovieSearch onSelectMovie={setSelectedMovie} />
-        
+        <SelectedMovieInfo movie={selectedMovie} />
         <Recommendations movieID={selectedMovie?.id ?? null} />
       </div>
     </main>
