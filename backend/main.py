29e43032f89d527658442fbfde2f90c088636741
Sophie <sophiie.tran@gmail.com>
2026-06-03 11:47:48 -0500

    add path permission

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    # add in website URL
+    allow_origins=["http://localhost:5173", "https://next-flick-phi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
